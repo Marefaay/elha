9e8ced228090d5c158dd9f66083cfd889ce94cbf
Mahmoud Refaay <125451007+Marefaay@users.noreply.github.com>
2024-08-05 14:32:10 +0300

    Update ContactPage.js

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -20,7 +20,7 @@ function ContactPage(props) {
           </div>
          <div className="map mt-5">
           <h5>Map</h5>
-         <iframe
+         <iframe title="location"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d361.9452331890151!2d31.21096120609935!3d30.429065579277207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x145875a3fa9298b7%3A0xfea69a52fd9c2cd6!2z2KfZhNi02YXZiNiq2Iwg2KjZhtmH2KfYjCDZhdit2KfZgdi42Kkg2KfZhNmC2YTZitmI2KjZitip!5e0!3m2!1sar!2seg!4v1720523194169!5m2!1sar!2seg"
             width="1000"
             height="400"
