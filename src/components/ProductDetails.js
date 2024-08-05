e7653bb7802c910b0fce6be6be20418f6e5fb2af
Mahmoud Refaay <125451007+Marefaay@users.noreply.github.com>
2024-08-05 14:45:16 +0300

    Update ProductDetails.js

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../fontAwesome";
 import "./ProductDetails.css";
+
 function ProductDetails(props) {
   props.showNavAndFoot(true);
   const params = useParams();
@@ -10,26 +11,26 @@ function ProductDetails(props) {
   console.log(params.productId);
   const url = "https://fakestoreapi.com/products";
   console.log(`${url}/${params.productId}`);
+  
   useEffect(() => {
     fetch(`${url}/${params.productId}`)
       .then((response) => response.json())
       .then((data) => setProduct(data))
       .catch((err) => console.log(err));
-  }, []);
-  // const rate = product.rating.rate;
-  // console.log(rate);
+  }, [params.productId]); // Added params.productId as a dependency
+
   return (
     <div className="product-details mt-4">
       <div className="container">
         <div className="row">
           <div className="col-lg-6">
-            <div className="product-image mt-5 ">
-              <img src={product.image} className="img-fluid" />
+            <div className="product-image mt-5">
+              <img src={product.image} className="img-fluid" alt={product.title} /> {/* Added alt attribute */}
             </div>
           </div>
           <div className="col-lg-6">
-            <div className="product-text mt-5 ">
-              <h1 className="mb-4"> {product.title}</h1>
+            <div className="product-text mt-5">
+              <h1 className="mb-4">{product.title}</h1>
               <h1 className="mb-5">${product.price}</h1>
 
               <button id="add-to-cart" onClick={() => props.addToCart(product)}>
@@ -43,7 +44,7 @@ function ProductDetails(props) {
               </button>
             </div>
             <span id="spacer"></span>
-            <div className="product-description ">
+            <div className="product-description">
               <p>{product.description}</p>
             </div>
           </div>
@@ -52,4 +53,5 @@ function ProductDetails(props) {
     </div>
   );
 }
+
 export default ProductDetails;
